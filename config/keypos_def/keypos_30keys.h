b77967b37b1036750fe22d88fb2adfad57a3f7b0
Bharat Garhewal <warfareonly@gmail.com>
2025-03-08 23:44:51 +0100

    Update with LB4 and RB4 keys, remove LT4 and RT4
    Todo: update the input-gpio mappings in the overlay

--- config/keypos_def/keypos_30keys.h
+++ config/keypos_def/keypos_30keys.h
@@ -27,45 +27,45 @@
 /*                              30 KEY MATRIX / LAYOUT MAPPING
 
   ╭────────────────────┬────────────────────╮ ╭─────────────────────┬─────────────────────╮
-  │  0   1   2   3   4 │  5   6   7   8   9 │ │ LT4 LT3 LT2 LT1 LT0 │ RT0 RT1 RT2 RT3 RT4 │
-  │ 10  11  12  13  14 │ 15  16  17  18  19 │ │ LM4 LM3 LM2 LM1 LM0 │ RM0 RM1 RM2 RM3 RM4 │
-  ╰───╮ 20  21  22     │     23  24  25 ╭───╯ ╰───╮ LB3 LB2 LB1     │     RB1 RB2 RB3 ╭───╯
+  │      0   1   2   3 |  4   5   6   7     │ │     LT3 LT2 LT1 LT0 │ RT0 RT1 RT2 RT3     │
+  │  8   9  10  11  12 │ 13  14  15  16  17 │ │ LM4 LM3 LM2 LM1 LM0 │ RM0 RM1 RM2 RM3 RM4 │
+  ╰ 18  19  20  21     │     22  23  24  25   ╰─LB4 LB3 LB2 LB1     │     RB1 RB2 RB3 RB4
       ╰───────╮ 26  27 │ 28  29 ╭───────╯         ╰───────╮ LH1 LH0 │ RH0 RH1 ╭───────╯
               ╰────────┴────────╯                         ╰─────────┴─────────╯             */
 
 #pragma once
 
-#define LT0 4 // left-top row
-#define LT1 3
-#define LT2 2
-#define LT3 1
-#define LT4 0
+#define LT0 3 // left-top row
+#define LT1 2
+#define LT2 1
+#define LT3 0
 
-#define RT0 5 // right-top row
-#define RT1 6
-#define RT2 7
-#define RT3 8
-#define RT4 9
+#define RT0 4 // right-top row
+#define RT1 5
+#define RT2 6
+#define RT3 7
 
-#define LM0 14 // left-middle row
-#define LM1 13
-#define LM2 12
-#define LM3 11
-#define LM4 10
+#define LM0 12 // left-middle row
+#define LM1 11
+#define LM2 10
+#define LM3 9
+#define LM4 8
 
-#define RM0 15 // right-middle row
-#define RM1 16
-#define RM2 17
-#define RM3 18
-#define RM4 19
+#define RM0 13 // right-middle row
+#define RM1 14
+#define RM2 15
+#define RM3 16
+#define RM4 17
 
-#define LB1 22 // left-bottom row
-#define LB2 21
-#define LB3 20
+#define LB1 21 // left-bottom row
+#define LB2 20
+#define LB3 19
+#define LB4 18
 
-#define RB1 23 // right-bottom row
-#define RB2 24
-#define RB3 25
+#define RB1 22 // right-bottom row
+#define RB2 23
+#define RB3 24
+#define RB4 25
 
 #define LH0 27 // left thumb keys
 #define LH1 26
@@ -73,17 +73,17 @@
 #define RH0 28 // right thumb keys
 #define RH1 29
 
-#define KEYS_L LT0 LT1 LT2 LT3 LT4 LM0 LM1 LM2 LM3 LM4 LB1 LB2 LB3 // left-hand keys
-#define KEYS_R RT0 RT1 RT2 RT3 RT4 RM0 RM1 RM2 RM3 RM4 RB1 RB2 RB3 // right-hand keys
+#define KEYS_L LT0 LT1 LT2 LT3 LM0 LM1 LM2 LM3 LM4 LB1 LB2 LB3 LB4 // left-hand keys
+#define KEYS_R RT0 RT1 RT2 RT3 RM0 RM1 RM2 RM3 RM4 RB1 RB2 RB3 RB4 // right-hand keys
 #define THUMBS LH1 LH0 RH0 RH1                                     // thumb keys
                                                                    //
 
-#define LAYER_FROM30(                                 \
-    k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, \
-    k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, \
-    k21, k22, k23, k26, k27, k28,                     \
-    k31, k32, k33, k34)                               \
-    k00 k01 k02 k03 k04 k05 k06 k07 k08 k09           \
-        k10 k11 k12 k13 k14 k15 k16 k17 k18 k19       \
-            k21 k22 k23 k26 k27 k28                   \
-                k31 k32 k33 k34
+// #define LAYER_FROM30(                                 \
+//     k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, \
+//     k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, \
+//     k21, k22, k23, k26, k27, k28,                     \
+//     k31, k32, k33, k34)                               \
+//     k00 k01 k02 k03 k04 k05 k06 k07 k08 k09           \
+//         k10 k11 k12 k13 k14 k15 k16 k17 k18 k19       \
+//             k21 k22 k23 k26 k27 k28                   \
+//                 k31 k32 k33 k34
